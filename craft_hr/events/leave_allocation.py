60c25b74a68e9b4acba8b67501e6e5636993ca2c
Craft Interactive <67686213+Craftint@users.noreply.github.com>
2024-03-28 12:22:03 +0400

    Update leave_allocation.py

--- craft_hr/events/leave_allocation.py
+++ craft_hr/events/leave_allocation.py
@@ -3,15 +3,7 @@ from craft_hr.events.get_leaves import get_leaves, get_earned_leave
 
 def validate(doc,method):
     # TODO: use is_earned_leave from inside leave type for this logic
-    if doc.custom_is_earned_leave:
-        total_opening_leaves = get_leaves(doc.custom_date_of_joining,doc.from_date, doc.custom_leave_distribution_template) or 0
-        doc.custom_used_leaves = total_opening_leaves - doc.custom_opening_leaves
-        doc.custom_opening_used_leaves = total_opening_leaves - doc.custom_opening_leaves
-        doc.new_leaves_allocated = doc.custom_opening_leaves
-        doc.custom_available_leaves = doc.custom_opening_leaves
-
-def before_submit(doc,method):
-    if doc.custom_is_earned_leave:
+    if doc.custom_is_earned_leave and doc.custom_leave_distribution_template:
         total_opening_leaves = get_leaves(doc.custom_date_of_joining,doc.from_date, doc.custom_leave_distribution_template) or 0
         doc.custom_used_leaves = total_opening_leaves - doc.custom_opening_leaves
         doc.custom_opening_used_leaves = total_opening_leaves - doc.custom_opening_leaves
