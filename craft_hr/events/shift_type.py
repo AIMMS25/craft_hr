43879980105f9ea50f22f5001c5f66e0fe0d3036
rayen <rayenlabdouni@gmail.com>
2024-07-08 10:47:43 +0400

    checking

--- craft_hr/events/shift_type.py
+++ craft_hr/events/shift_type.py
@@ -4,8 +4,11 @@ from frappe.utils import (time_diff_in_hours)
 from datetime import timedelta
 
 def before_validate(doc, method):
-    start = datetime.datetime.strptime(doc.start_time, "%H:%M:%S")
-    end = datetime.datetime.strptime(doc.end_time, "%H:%M:%S")
+    start = doc.start_time
+    end = doc.end_time
+    if type(doc.start_time) == str:
+        start = datetime.datetime.strptime(doc.start_time, "%H:%M:%S")
+        end = datetime.datetime.strptime(doc.end_time, "%H:%M:%S")
     if doc.is_night_shift:
         end += timedelta(hours=24)
     shift_hours = time_diff_in_hours(end,start)
