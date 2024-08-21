5f173bccc1667c462ad2988a80900291b3ca694f
Craft Interactive <67686213+Craftint@users.noreply.github.com>
2024-08-21 18:53:55 +0530

    fix: db_set used since data not getting saved

--- craft_hr/events/leave_allocation.py
+++ craft_hr/events/leave_allocation.py
@@ -3,12 +3,13 @@ from craft_hr.events.get_leaves import get_leaves, get_earned_leave
 
 def validate(doc,method):
     # TODO: use is_earned_leave from inside leave type for this logic
-    if doc.custom_is_earned_leave and doc.custom_leave_distribution_template:
+    if doc.custom_is_earned_leave and doc.custom_leave_distribution_template and doc.docstatus = 0:
         total_opening_leaves = get_leaves(doc.custom_date_of_joining,doc.from_date, doc.custom_leave_distribution_template) or 0
-        doc.custom_used_leaves = total_opening_leaves - doc.custom_opening_leaves
-        doc.custom_opening_used_leaves = total_opening_leaves - doc.custom_opening_leaves
-        doc.new_leaves_allocated = doc.custom_opening_leaves
-        doc.custom_available_leaves = doc.custom_opening_leaves
+        used_leaves = total_opening_leaves - doc.custom_opening_leaves
+        doc.db_set("custom_used_leaves", used_leaves)
+        doc.db_set("custom_opening_used_leaves", used_leaves)
+        doc.db_set("new_leaves_allocated", doc.custom_opening_leaves)
+        doc.db_set("custom_available_leaves", doc.custom_opening_leaves)
 
 def before_submit(doc,method):
     if doc.custom_is_earned_leave:
