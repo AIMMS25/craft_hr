e925b8d8551ca9bba121edd1b1b7027f674f121a
Craft Interactive <67686213+Craftint@users.noreply.github.com>
2024-11-07 12:52:51 +0400

    add Leave Encashment

--- craft_hr/hooks.py
+++ craft_hr/hooks.py
@@ -123,7 +123,7 @@ after_install = "craft_hr.install.after_install"
 
 override_doctype_class = {
 	# "ToDo": "custom_app.overrides.CustomToDo"
-	# "Leave Encashment":"craft_hr.overrides.leave_encashment.CustomLeaveEncashment",
+	 "Leave Encashment":"craft_hr.overrides.leave_encashment.CustomLeaveEncashment",
 
 
  
