80973c1105b61bef74ded155a8d0ab38008c4902
gayathrik001 <gayathrik2g@gmail.com>
2024-11-12 16:40:27 +0530

    fix:Employee Time Sheet Summary

---
+++ craft_hr/craft_hr/report/employee_time_sheet_summary/employee_time_sheet_summary.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2024, Craftinteractive and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Employee Time Sheet Summary"] = {
+	"filters": [
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+		},
+		{
+            "fieldname": "from_date",
+            "label": __("From date"),
+            "fieldtype": "Date",
+        },
+		{
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+        },
+		{
+            "fieldname": "summarized_view",
+            "label": __("Summarized View"),
+            "fieldtype": "Check",
+        },
+
+
+	]
+};
