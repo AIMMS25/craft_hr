f355ae8c46f172eef4c3d3053c69538784542642
Meghakt123 <ktmegha10@gmail.com>
2025-02-18 11:09:05 +0530

    fix: duplicate entry validation in monthly overtime sheet

--- craft_hr/craft_hr/doctype/monthly_overtime_sheet/monthly_overtime_sheet.js
+++ craft_hr/craft_hr/doctype/monthly_overtime_sheet/monthly_overtime_sheet.js
@@ -1,31 +1,33 @@
-// Copyright (c) 2023, Craftinteractive and contributors
-// For license information, please see license.txt
-
+// // Copyright (c) 2023, Craftinteractive and contributors
+// // For license information, please see license.txt
 
 
 frappe.ui.form.on('Monthly Overtime Sheet', {
-    get_employees(frm){
-        if (frm.is_new()){
+    get_employees(frm) {
+        if (frm.is_new()) {
             frappe.db.get_list('Employee', {
-                fields: ['name','employee_name','department'],
+                fields: ['name', 'employee_name', 'department'],
                 filters: {
                     status: 'Active',
                     company: frm.doc.company
                 },
-                limit:0,
-                order_by:'name asc'
+                limit: 0,
+                order_by: 'name asc'
             }).then(records => {
-                records.forEach(emp=>{
-                    frm.add_child('ot_table',{
-                        employee:emp.name,
-                        employee_name:emp.employee_name,
-                        department:emp.department,
-                        date:frm.doc.date
-                        
-                    });
-                    frm.refresh_fields('ot_table');
+                let existing_employees = frm.doc.ot_table.map(row => row.employee);
+                
+                records.forEach(emp => {
+                    if (!existing_employees.includes(emp.name)) {
+                        frm.add_child('ot_table', {
+                            employee: emp.name,
+                            employee_name: emp.employee_name,
+                            department: emp.department,
+                            date: frm.doc.date
+                        });
+                    }
+                });
+                frm.refresh_fields('ot_table');
             });
-		});
         }
     },
 });
